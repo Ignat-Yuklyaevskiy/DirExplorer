e7f2b4591faf5c37164ced20170ebed64e146c51
Пользователь <@LAPTOP-HSRE746E>
2022-12-07 19:11:49 +0300

    Add project files.

---
+++ FileAnalyser.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <string>
+
+
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::string path = "C:/";
+	for (const auto& item : fs::directory_iterator(path))
+	{
+		std::cout << item.path() << std::endl;
+	}
+    return 0;
+}
